b890eb2246112642cae92d221d7691244023093d
Raja-002 <154741441+Raja-002@users.noreply.github.com>
2024-04-26 21:02:21 +0530

    Add files via upload

---
+++ Javascript/color flipper/script.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById("btn");
+const colortext = document.getElementById("color");
+const wrap = document.getElementById("wrap");
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+
+btn.addEventListener('click', function () {
+    let hexcolor = '#';
+    for (let i = 1; i <= 6; i++) {
+        hexcolor = hexcolor + randhexvalue();
+    }
+    
+
+    // set the color while clicking
+    wrap.style.backgroundColor = hexcolor;
+    colortext.innerHTML = hexcolor;
+});
+
+`   `
+
+function randhexvalue() {
+    let randomIndex = Math.floor(Math.random() * 16);
+    return hex[randomIndex];
+}
