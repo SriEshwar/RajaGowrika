c252d8a84a40308b5190ce61ca3b331c8479a605
Raja-002 <154741441+Raja-002@users.noreply.github.com>
2024-04-26 09:57:02 +0530

    Add files via upload

---
+++ login/index.js
@@ -0,0 +1,20 @@
+function validatePassword() {
+    var p = document.getElementById('password').value
+    console.log("xfcgvhbjn")
+        errors = [];
+    if (p.length < 8) {
+        errors.push("Your password must be at least 8 characters"); 
+    }
+    if (p.search(/[a-z]/i) < 0) {
+        errors.push("Your password must contain at least one letter.");
+    }
+    if (p.search(/[0-9]/) < 0) {
+        errors.push("Your password must contain at least one digit."); 
+    }
+    if (errors.length > 0) {
+        alert(errors.join("\n"));
+        return false;
+    }
+    return true;
+    alert("Successfully Created Account!");
+}
