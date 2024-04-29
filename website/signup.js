fd0b32c10ccd03e227b9bd1c5e677b9f1603c8e4
Raja-002 <154741441+Raja-002@users.noreply.github.com>
2024-04-29 11:04:56 +0530

    Create signup.js

---
+++ website/signup.js
@@ -0,0 +1,16 @@
+
+document.getElementById("signup-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+   
+    console.log("New User Details:");
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    
+});
