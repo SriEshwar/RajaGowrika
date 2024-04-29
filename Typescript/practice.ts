b800fec93841f330ee332165983367246b14b8bd
Raja-002 <154741441+Raja-002@users.noreply.github.com>
2024-04-29 22:01:26 +0530

    Create practice.ts

---
+++ Typescript/practice.ts
@@ -0,0 +1,168 @@
+/*var greeter = "hey hi";  
+var times = 5;  
+if (times > 3) {  
+   var greeter = "Hi";   
+}  
+console.log(greeter) 
+
+let greeter = "hey hi";  
+let times = 5;  
+if (times > 3) {  
+   let hello = "Say Hello";   
+   console.log(hello)   
+}  
+console.log(hello)*/
+
+/*
+function greeter(person) {  
+    return "Hello, " + person;  
+}  
+let user = 'All';  
+console.log(greeter(user));  
+
+function addNumbers(a, b) {  
+    return a + b;  
+}  
+var sum = addNumbers("Typescript", 25);  
+console.log('Sum of the numbers is: ' + sum);  
+
+
+let a = 10;
+let b = 20;
+console.log(a===b);    
+console.log(a===10);   
+console.log(10==='10')
+
+let arrTuple = [101, "Typescript", 105, "Javascript"];  
+console.log(arrTuple);
+
+
+let arrTuple = [];   
+arrTuple[0] = 101  
+arrTuple[1] = 105  
+console.log(arrTuple)
+
+//Push()
+let technology = ["Javascript", 25, "python"];  
+console.log("Items: "+technology);  
+console.log("Length of Tuple Items before push: "+technology.length);    
+technology.push("java"); 
+console.log("Length of Tuple Items after push: "+technology.length);  
+console.log("Items: "+technology);
+
+//pop()
+technology.pop();   // removed value to the tuple   
+console.log("Length of Tuple Items after pop: "+technology.length);  
+console.log("Items: "+technology);  
+
+//update
+technology[1] = 30;
+console.log(technology);
+
+//clear
+technology = [];  
+
+//number
+
+let first: number = 12.0;               
+let second: number = 0x37CF;            
+let third: number = 0o377 ;          
+let fourth: number = 0b111001;        
+  
+console.log(first);          
+console.log(second);          
+console.log(third);        
+console.log(fourth);  
+
+//Map
+
+let map = new Map();  
+  
+map.set('1', 'java');     
+map.set(1, 'python');       
+map.set(3, 'c');   
+map.set('2', 'php');  
+  
+console.log( "Value1= " +map.get(1)   );   
+console.log("Value2= " + map.get('1') );   
+console.log( "Key is Present= " +map.has(3) );   
+console.log( "Size= " +map.size );   
+console.log( "Delete value= " +map.delete(1) );   
+console.log( "New Size= " +map.size );  
+
+//generate keys
+for (let key of map.keys()) {  
+    console.log("Map Keys= " +key);   
+
+//values
+for (let value of map.values()) {  
+    console.log("Map Values= " +value);   
+
+//Iterate over all map
+for (let entry of map.entries()) {  
+    console.log(entry[0], entry[1]);   
+
+
+//Set
+let set = new Set();  
+   
+//Add Values  
+set.add("java");  
+set.add("python");  
+set.add("c");  
+set.add("c++");   
+set.add("html");   
+   
+console.log(set);   
+   
+console.log(set.has("java"));        
+console.log(set.has(10));        
+   
+console.log(set.size);    
+   
+console.log(set.delete("Dhawan"));      
+     
+set.clear();   
+   
+console.log(set);  
+
+//Iteration
+
+for (let val of set) {  
+    console.log(val);   
+}  
+
+
+//ForEach
+
+var num = [5, 10, 15];  
+num.forEach(function (value) {  
+  console.log(value);  
+});  
+
+let apps = ['WhatsApp', 'Instagram', 'Facebook'];  
+let playStore = [];  
+  
+apps.forEach(function(item){  
+  playStore.push(item)  
+});  
+  
+console.log(playStore);  
+
+//Functions
+
+function display() {  
+    console.log("Hello");  
+}    
+display();  
+
+
+let myAdd = function (x: number, y: number) : number {  
+    return x + y;  
+};  
+ 
+console.log(myAdd(5,3));  
+
+
+
+*/
